7c3e0741163528dd7e36a1fc8ec927785f8fb5f3
HassanSalah01 <hassan.salah.eldine1@gmail.com>
2024-01-23 21:39:49 +0200

    Added-main-file

---
+++ main.js
@@ -0,0 +1,53 @@
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+let player;
+let background;
+let floor;
+let enemy;
+
+const draw = () => {
+  enemy = new Enemy({
+    x: 700,
+    y: 550,
+    w: 20,
+    r: 50,
+    color: "yellow",
+    ctx: ctx,
+  });
+  floor = new Player({
+    x: 0 + 10,
+    y: 600,
+    w: 800 - 20,
+    r: 10,
+    color: "white",
+    ctx: ctx,
+  });
+  background = new Player({
+    x: 0,
+    y: 0,
+    w: canvas.width,
+    r: canvas.height,
+    color: "black",
+    ctx: ctx,
+  });
+  // player = new Player({
+  //   x: 100,
+  //   y: 550,
+  //   w: 50,
+  //   r: 50,
+  //   color: "red",
+  //   ctx: ctx,
+  // });
+  // console.log(player);
+};
+
+const update = () => {
+  background.draw();
+  enemy.draw();
+  enemy.update();
+  // player.draw();
+  floor.draw();
+  window.requestAnimationFrame(update);
+};
+draw();
+update();
